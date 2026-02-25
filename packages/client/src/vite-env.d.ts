d0cb920963921d5aaa52e64d31b1866347efb3a3
Dima <ivanchenko.dim@gmail.com>
2026-02-25 10:49:34 +0200

    update

---
+++ packages/client/src/vite-env.d.ts
@@ -0,0 +1,2 @@
+// packages/client/src/vite-env.d.ts
+/// <reference types="vite/client" />
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@blind/shared': path.resolve(__dirname, '../shared/src'),
+      '@blind/shared': path.resolve(__dirname, '../shared/src/index.ts'),
     },
   },
   server: {
